9b4b732f6d7e29d7852e7010a2614a4a75869477
kimntan <kim.thd.nguyen@gmail.com>
2024-09-26 10:14:22 -0400

    Add spend and save prompt.

--- client/src/components/TransactionBlock/TransactionBlock.tsx
+++ client/src/components/TransactionBlock/TransactionBlock.tsx
@@ -4,6 +4,11 @@ export default function TransactionBlock() {
   return (
     <div className="transaction">
       <h3 className="transaction__heading">Would you like to</h3>
+      <div className="transaction__buttons">
+        <button className="transaction__save-button">save</button>
+        <span className='transaction__or'>or</span>
+        <button className="transaction__spend-button">spend</button>
+      </div>
     </div>
   )
 }
